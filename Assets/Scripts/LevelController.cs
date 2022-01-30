097f4b7b2e09979aab6f29f24e7a484fb75e1f40
Jose Fernandez <jafp20@gmail.com>
2022-01-29 23:21:59 -0300

    Create level controller to handle difficulty

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,42 +4,15 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
-    // Script Objects
     PlayerScore scoreScript;
     Spawner spawner;
-    Potions sanityScript;
-    Potions crazyScript;
-    Launcher launcherScript;
-    // Current time
     float timer;
-    // Current score
     float currentScore = 0;
-    // Last score saved
     float lastScore = 0;
 
-    // Time in seconds to trigger every change
-    public float timeFlag = 60;
-    // Quantity of points to trigger every change
-    public float pointFlag = 100;
-    // Name of generator object
+    public float timeFlag = 30;
+    public float pointFlag = 10;
     public string generatorName = "Generator";
-    public string launcherName = "Launcher";
-
-    void Start() {
-        // Inntiliaze Score script
-        Camera camera = Camera.main;
-        scoreScript = camera.GetComponent<PlayerScore>();
-
-        // Innitialize generator potions
-        GameObject generator = GameObject.Find(generatorName);
-        spawner = generator.GetComponent<Spawner>();
-        sanityScript = spawner.sanityPotion.GetComponent<Potions>();
-        crazyScript = spawner.crazyPotion.GetComponent<Potions>();
-
-        // Innitialize launcher 
-        GameObject launcher = GameObject.Find(launcherName);
-        launcherScript = launcher.GetComponent<Launcher>();
-    }
 
     // Update is called once per frame
     void Update() {
@@ -54,22 +27,30 @@ public class LevelController : MonoBehaviour
         }
     }
 
-    // Method to get score from main camera script
     void getPlayerScore() {
+        Camera camera = Camera.main;
+
+        scoreScript = camera.GetComponent<PlayerScore>();
+
         currentScore = scoreScript.score;
     }
 
-    // Method to overwrite to generator script and potion scripts the new values
     void handlerLevelIncrease () {
-        if(launcherScript.interval > .5f){
-            launcherScript.interval -= .1f;
-        }
+        GameObject generator = GameObject.Find(generatorName);
+
+        spawner = generator.GetComponent<Spawner>();
+
+        Potions sanityScript = spawner.sanityPotion.GetComponent<Potions>();
+        Potions crazyScript = spawner.crazyPotion.GetComponent<Potions>();
+
         if(spawner.interval > .5f){
-            spawner.interval -= .1f;
+            spawner.interval -= - .1f;
         }
-        if(sanityScript.time > .3f){
+
+        if(sanityScript.time > .5f){
             sanityScript.time -= .2f;
         }
+
         crazyScript.time += .2f;
     }
 }
